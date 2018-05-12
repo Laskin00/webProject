5ceafca2e1e0fb760547b93377f59770cf260e1d
Laskin00 <ale.laskintues19@gmail.com>
2018-05-12 11:44:36 +0300

    fixes

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # webProject
-<<<<<<< HEAD
-=======
 # webProject
->>>>>>> aba0a35dfe4269bc026d0732ccb75bb8600c8b10
--- navbar.js
+++ navbar.js
@@ -1,16 +1,32 @@
 function insertNavbar(){
-  var navbar = "<ul class = " + "nav"+ "><li class = " + "home" + "><a href='Landingpage.html'>Home</a></li><li class = 'nav_li'><a href='Jluch.html'>Жлъч</a></li><li class = 'nav_li'><a href='SoCalledCrew.html'>SoCalledCrew</a></li><li class = 'nav_li'><a href='Boro.html'>Боро Първи</a></li><li class = 'nav_li'><a href='Toshiba.html'>Григовор</a></li></ul>";
+  var navbar = "<ul class = " + "nav"+ "><li class = " + "home" + "><a href='Landingpage.html'>Home</a></li><li class = 'nav_li'><a href='Jluch.html'>Жлъч</a></li><li class = 'nav_li'><a href='SoCalledCrew.html'>SoCalledCrew</a></li><li class = 'nav_li'><a href='Boro.html'>Боро Първи</a></li><li class = 'nav_li'><a href='Grigovor.html'>Григовор</a></li></ul>";
   var element = document.getElementById('nav');
   element.innerHTML = navbar;
   document.body.appendChild(element);
 }
+
 function swapText(text){
   var element = document.getElementById('song_txt');
-  element.innerHTML = text;
-  element.css("text-align", "center")
-  document.body.appendChild(element);
+   element.innerHTML = text;
+   element.css("text-align", "center")
+   document.body.appendChild(element);
+  // var r = new FileReader();
+  // r.onload = (function(text) {
+  //     return function(e) {
+  //         var contents = e.target.result;
+  //     };
+  // })(text);
+  // var element = document.getElementById('song_txt');
+  // element.innerText = r.readAsText(text);
+  // element.css("text-align", "center");
+  // document.body.appendChild(element);
+
+
 }
-//
-// <ul class="nav">
-// 	<li class="home" id = "navbar_li"><a href = "Landingpage">Home</a></li>
-// </ul>
+
+function processFile(theFile){
+  return function(e) {
+    var theBytes = e.target.result;
+    document.getElementById('file').innerText = theBytes;
+  }
+}
